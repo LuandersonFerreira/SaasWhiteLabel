26c139cadf54eabb8f06a3e342bd64e7ad18a0f7
Leanderson <leandersonferreira12@gmail.com>
2025-04-06 05:53:31 -0300

    resolving conflicts

--- src/RouteConfig.jsx
+++ src/RouteConfig.jsx
@@ -1,22 +1,23 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Views/Home/index.jsx";
+import Login from "./Views/Login/index.jsx";
 import CreateInvite from "./Views/CreateInvite/index.jsx";
 import Invite from "./Views/Invite/index.jsx";
 import HomeLayout from "./layout/Home/index.jsx";
-import Login from "./Views/Login/Login.jsx";
+import Event from "./Views/Event/index.jsx";
 
 const RouteConfig = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<h1> Not Found </h1>} />
-        <Route path="/Login" element={<Login />} />
-
         <Route path="/" element={<HomeLayout />}>
           <Route index element={<Home />} />
           <Route path="/Event/create-invite" element={<CreateInvite />} />
+          <Route path="/Event/:eventId" element={<Event />} />
           <Route path="/invite/:inviteId" element={<Invite />} />
         </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
